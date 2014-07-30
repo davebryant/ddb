d415c22a7dc362669144d58b9276da8e928beae0
davebryant <davebryant@gmail.com>
2014-07-30 07:36:01 -0400

    Initial commit

---
+++ trunk/ddb-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ddb_web.Startup))]
+namespace ddb_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
